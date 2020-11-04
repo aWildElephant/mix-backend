27bc185e95350a1d57ef1856f711a306d36c4dad
Étienne Girard <etienne.g.girard@gmail.com>
2020-11-04 23:08:21 +0100

    Initial commit (spring initializr)

---
+++ src/main/java/fr/awildelephant/mmixbackend/MmixBackendApplication.java
@@ -0,0 +1,13 @@
+package fr.awildelephant.mmixbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MmixBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MmixBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/awildelephant/mmixbackend/MmixBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.awildelephant.mmixbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MmixBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
