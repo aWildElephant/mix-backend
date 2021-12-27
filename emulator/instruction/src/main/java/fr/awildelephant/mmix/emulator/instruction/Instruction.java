db3a771e076778649322a90868d3e53a2c9db7f1
Étienne Girard <etienne.g.girard@gmail.com>
2021-12-27 22:56:00 +0100

    Add operations enum with operation code/modification
    - add missing tokens for operations in parser

--- emulator/instruction/src/main/java/fr/awildelephant/mmix/emulator/instruction/Instruction.java
+++ emulator/instruction/src/main/java/fr/awildelephant/mmix/emulator/instruction/Instruction.java
@@ -1,9 +1,14 @@
 package fr.awildelephant.mmix.emulator.instruction;
 
+import lombok.Builder;
 import lombok.Value;
 
 @Value
+@Builder
 public class Instruction {
 
-    long content;
+    Operation operation;
+    byte modification;
+    byte indexSpecification;
+    Address address;
 }
