3610a4206c673dbec8a5b4ad6b5ce7a4ba6b40c5
Etienne Girard <etienne.g.girard@gmail.com>
2025-08-22 15:14:51 +0200

    Add tests for ADD and SUB

---
+++ emulator/engine/src/test/java/fr/awildelephant/mix/emulator/engine/executor/ADDExecutorTest.java
@@ -0,0 +1,27 @@
+package fr.awildelephant.mix.emulator.engine.executor;
+
+import fr.awildelephant.mix.emulator.engine.helper.WordStringRepresentation;
+import fr.awildelephant.mix.emulator.engine.state.Machine;
+import fr.awildelephant.mix.emulator.engine.state.MachineBuilder;
+import org.junit.jupiter.api.Test;
+
+import static fr.awildelephant.mix.emulator.engine.helper.ExecutorTestHelper.execute;
+import static fr.awildelephant.mix.emulator.engine.testutil.MachineAsserter.assertThatMachine;
+
+public class ADDExecutorTest {
+
+    /**
+     * page 132 ADD example
+     */
+    @Test
+    void test1() {
+        final Machine machine = MachineBuilder.builder()
+                .withARegister(WordStringRepresentation.toWord("[+|,1234|1|,150]"))
+                .withMemoryCell(1000, WordStringRepresentation.toWord("[+|,100|5|,50]"))
+                .build();
+
+        execute("ADD 1000", machine);
+
+        assertThatMachine(machine).hasRegisterA("[+|,1334|6|,200]");
+    }
+}
---
+++ emulator/engine/src/test/java/fr/awildelephant/mix/emulator/engine/executor/SUBExecutorTest.java
@@ -0,0 +1,28 @@
+package fr.awildelephant.mix.emulator.engine.executor;
+
+import fr.awildelephant.mix.emulator.engine.helper.WordStringRepresentation;
+import fr.awildelephant.mix.emulator.engine.state.Machine;
+import fr.awildelephant.mix.emulator.engine.state.MachineBuilder;
+import org.junit.jupiter.api.Test;
+
+import static fr.awildelephant.mix.emulator.engine.helper.ExecutorTestHelper.execute;
+import static fr.awildelephant.mix.emulator.engine.testutil.MachineAsserter.assertThatMachine;
+
+public class SUBExecutorTest {
+
+    /**
+     * page 132 SUB example
+     */
+    @Test
+    void test1() {
+        final Machine machine = MachineBuilder.builder()
+                .withARegister(WordStringRepresentation.toWord("[-|,1234|0|0|9]"))
+                .withMemoryCell(1000, WordStringRepresentation.toWord("[-|,2000|,150|0]"))
+                .build();
+
+        execute("SUB 1000", machine);
+
+        // last value is actually undefined in the example, but we produce a deterministic result
+        assertThatMachine(machine).hasRegisterA("[+|,766|,149|55]");
+    }
+}
